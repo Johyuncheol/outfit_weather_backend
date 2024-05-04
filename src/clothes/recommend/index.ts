529de66318746aa29e3953638fd4f41311c7d242
Johyuncheol <finos3703@naver.com>
2024-05-04 12:31:04 +0900

    [fix]: recommend에서 메인아이템 개수 메인아이템 선정 개수 로직 변경 -> 반복문을 사용한 무작위에서 -> findOuterLength+findTopLength+findInnerLength로 해당하는 모든 아이템을 가져오도록 수정
    isRandom삭제
    -> 논리오류 , 필요없는 부분이였음

--- src/clothes/recommend/index.ts
+++ src/clothes/recommend/index.ts
@@ -79,25 +79,21 @@ router.get("/", async (req: Request, res: Response) => {
   }
 
   // 무작위로 뽑는 함수
-  function getRandomValue(arr: any[]) {
+  function getRandomValue(arr: ItemType[]) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
   }
 
-  function getCombination(findData: any, fixedCategory: string): any {
-    const newComb: any = {};
-    const newArray: any = [];
+  function getCombination(findData: FindDataType, fixedCategory: string) {
+    const newComb: NewCombType = {};
+    const newArray: NewCombType[] = [];
     const RecommendData: any = {};
-    let isRandom = false;
 
-    const prev: any = {};
     // 고정아이템 설정
-
     newComb[fixedCategory] = getRandomValue(findData[fixedCategory]);
-    prev[fixedCategory] = newComb[fixedCategory];
 
     // 고정카테고리를 제외한 나머지로 설정
-    let otherCategories: any[] = [];
+    let otherCategories: string[] = [];
     if (fixedCategory === "outer") {
       otherCategories = ["top", "inner", "bottom"];
     } else if (fixedCategory === "top") {
@@ -108,31 +104,30 @@ router.get("/", async (req: Request, res: Response) => {
 
     const uniqueCombinations = new Set();
 
+    // 조합생성
     for (let i = 0; i < 10; i++) {
       otherCategories.forEach((category) => {
+        // 찾은 데이터의 weight 가중치가 높은 순으로 추출
         let item = newComb[fixedCategory].weight[category];
         item.sort((a: any, b: any) => b.count - a.count);
 
+        // 찾은 데이터의 weight 가중치가 높은 순으로 추출
+        // 가중치 객체에서 가져올 데이터가 없다면 해당 카테고리 아이템 전체에서 랜덤으로 가져옴
+        // weight의 아이템보다 추출하는 데이터가 많다면 카테고리 가중치 배열에서 무작위 추출
         if (item.length - 1 >= i) {
           newComb[category] = item[i];
-          isRandom = false;
         } else if (item.length === 0) {
           newComb[category] = getRandomValue(AllData[category]);
-          isRandom = true;
         } else {
           newComb[category] = getRandomValue(item);
-          isRandom = false;
         }
       });
 
       // 중복확인해서 중복아닐때만 추가 시킴
-      const combinationKey1 = JSON.stringify(newComb);
-      const combinationKey2 = JSON.stringify(prev);
-      if (
-        !uniqueCombinations.has(combinationKey1) &&
-        combinationKey1 !== combinationKey2
-      ) {
-        uniqueCombinations.add(combinationKey1);
+      const combinationKey = JSON.stringify(newComb);
+      if (!uniqueCombinations.has(combinationKey)) {
+        uniqueCombinations.add(combinationKey);
+
         newArray.push({ ...newComb });
       }
 
@@ -146,43 +141,38 @@ router.get("/", async (req: Request, res: Response) => {
     RecommendData.styles = newArray;
     const mainItem = newComb[fixedCategory];
 
-    return { RecommendData, mainItem, isRandom };
+    return { RecommendData, mainItem };
   }
 
-  const array: any[] = [];
+  const array: RecommendDataType[] = [];
 
-  let check: any = [];
+  let check: ItemType[] = [];
+  const findOuterLength = findData.outer.length;
+  const findTopLength = findData.top.length;
+  const findInnerLength = findData.inner.length;
 
-  // 가장 상위 카테고리 옷 찾기
+  // 기준이 될 아이템 카테고리 배열
   let fixedCategory = [];
-  if (findData.outer.length > 0) {
+  if (findOuterLength > 0) {
     fixedCategory.push("outer");
   }
-  if (findData.top.length > 0) {
+  if (findTopLength > 0) {
     fixedCategory.push("top");
   }
-  if (findData.inner.length > 0) {
+  if (findInnerLength > 0) {
     fixedCategory.push("inner");
   }
 
-  for (let i = 0; i < 4; i++) {
+  while (array.length < findOuterLength + findTopLength + findInnerLength) {
     for (const fixed of fixedCategory) {
-      const { RecommendData, mainItem, isRandom } = getCombination(
-        findData,
-        fixed
-      );
+      const { RecommendData, mainItem } = getCombination(findData, fixed);
+
       if (!check.includes(mainItem)) {
-        if (isRandom) {
-          array.push(RecommendData);
-        } else {
-          array.unshift(RecommendData);
-        }
+        array.push(RecommendData);
         check.push(mainItem);
       }
     }
   }
-
-
 
   return res.status(200).send({
     data: array,
@@ -191,3 +181,33 @@ router.get("/", async (req: Request, res: Response) => {
 });
 
 export default router;
+
+interface ItemType {
+  _id: { $oid: string };
+  category: string;
+  subcategory: string;
+  temp: string;
+  imgSrc: string;
+  name: string;
+  weight: {
+    outer: Array<any>;
+    top: Array<any>;
+    inner: Array<any>;
+    bottom: Array<any>;
+  };
+  __v: { $numberInt: string };
+}
+
+interface FindDataType {
+  [key: string]: ItemType[];
+}
+
+// getCombination 함수에서 사용되는 객체의 타입 정의
+interface NewCombType {
+  [key: string]: any;
+}
+
+interface RecommendDataType {
+  mainItem: ItemType;
+  styles: NewCombType[];
+}
